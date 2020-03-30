7f1764277f0555d247b408f23296258fa7f0e345
fbarda <arda.aydin@operationsilkscarf.com>
2020-03-30 19:15:49 +0300

    Added TypedArray support. Exported array conversion function to a global function.

--- asyncArrayUtils.mjs
+++ asyncArrayUtils.mjs
@@ -1,7 +1,13 @@
-export const some = async function asyncSome(array, asyncFunction) {
-    if (array instanceof Set) {
-        array = [...array];
+const arrayConverter = arrayLike => {
+    const convertAllowedTypes = [Set, Object.getPrototypeOf(Uint8Array)];
+    if (convertAllowedTypes.some(type => arrayLike instanceof type)) {
+        return [...arrayLike];
     }
+    return arrayLike;
+};
+
+export const some = async function asyncSome(array, asyncFunction) {
+    array = arrayConverter(array);
     let doesSomeResolveToTrue = false;
     for (let i = 0; i < array.length; i++) {
         doesSomeResolveToTrue = !!(await asyncFunction(array[i], i, array));
@@ -13,10 +19,10 @@ export const some = async function asyncSome(array, asyncFunction) {
 };
 export const mapConstructor = function asyncArrayMapConstructor(array) {
     const self = {
-        array,
+        array: [...arrayConverter(array)],
         map: async function (asyncFunction) {
-            for (let i = 0; i < array.length; i++) {
-                array[i] = await asyncFunction(array[i], i, array);
+            for (let i = 0; i < this.array.length; i++) {
+                this.array[i] = await asyncFunction(this.array[i], i, this.array);
             }
             return self;
         }
@@ -24,9 +30,7 @@ export const mapConstructor = function asyncArrayMapConstructor(array) {
     return self;
 };
 export const reduceRight = async function asyncReduceRight(array, asyncFunction, initialValue) {
-    if (array instanceof Set) {
-        array = [...array];
-    }
+    array = arrayConverter(array);
     let i = array.length - 1;
     if (typeof initialValue === "undefined") {
         initialValue = array[i];
@@ -37,9 +41,7 @@ export const reduceRight = async function asyncReduceRight(array, asyncFunction,
     }
 };
 export const reduce = async function asyncReduce(array, asyncFunction, initialValue) {
-    if (array instanceof Set) {
-        array = [...array];
-    }
+    array = arrayConverter(array);
     let i = 0;
     if (typeof initialValue === "undefined") {
         initialValue = array[i];
@@ -51,7 +53,7 @@ export const reduce = async function asyncReduce(array, asyncFunction, initialVa
     return initialValue;
 };
 export const forEach = async function asyncForEach(array, asyncFunction) {
-    const copiedArray = [...array];
+    const copiedArray = [...arrayConverter(array)];
     let promiseChain = Promise.resolve();
     for (let i = 0; i < copiedArray.length; i++) {
         await promiseChain; //added await here instead of end of the loop to prevent side-effects. May increase time spent on chain.
@@ -61,9 +63,7 @@ export const forEach = async function asyncForEach(array, asyncFunction) {
     return;
 };
 export const every = async function asyncEvery(array, asyncFunction) {
-    if (array instanceof Set) {
-        array = [...array];
-    }
+    array = arrayConverter(array);
     let didAnyFail = false;
     for (let i = 0; i < array.length; i++) {
         didAnyFail = !(await asyncFunction(array[i], i, array));
@@ -75,7 +75,7 @@ export const every = async function asyncEvery(array, asyncFunction) {
 };
 
 export const extendedIndexOf_sync = function deepSearch(array, syncFunction) {
-    if (array instanceof Set) { array = [...array]; }
+    array = arrayConverter(array);
     const defaultResult = -1;
     for (let i = 0; i < array.length; i++) {
         const result = syncFunction(array[i], i, array);
@@ -86,7 +86,7 @@ export const extendedIndexOf_sync = function deepSearch(array, syncFunction) {
     return defaultResult;
 };
 export const extendedIndexOf = async function deepSearchAsync(array, asyncFunction) {
-    if (array instanceof Set) { array = [...array] }
+    array = arrayConverter(array);
     const defaultResult = -1;
     for (let i = 0; i < array.length; i++) {
         const result = await asyncFunction(array[i], i, array);
@@ -95,9 +95,7 @@ export const extendedIndexOf = async function deepSearchAsync(array, asyncFuncti
     return defaultResult;
 };
 export const extendedLastIndexOf = async function deepSearchFromEndAsync(array, asyncFunction) {
-    if (array instanceof Set) {
-        array = [...array];
-    }
+    array = arrayConverter(array);
     const defaultResult = -1;
     for (let i = array.length; i > 0; i--) {
         const result = await asyncFunction(array[i - 1], i - 1, array);
@@ -106,9 +104,7 @@ export const extendedLastIndexOf = async function deepSearchFromEndAsync(array, 
     return defaultResult;
 };
 export const extendedLastIndexOf_sync = function deepSearchFromEnd(array, syncFunction) {
-    if (array instanceof Set) {
-        array = [...array];
-    }
+    array = arrayConverter(array);
     const defaultResult = -1;
     for (let i = array.length; i > 0; i--) {
         const result = syncFunction(array[i - 1], i - 1, array);
@@ -117,9 +113,7 @@ export const extendedLastIndexOf_sync = function deepSearchFromEnd(array, syncFu
     return defaultResult;
 };
 export const filter = async function filterAsync(array, asyncBooleanFunction) {
-    if (array instanceof Set) {
-        array = [...array];
-    }
+    array = arrayConverter(array);
     const filteredArray = [];
     for (let i = 0; i < array.length; i++) {
         if (await asyncBooleanFunction(array[i], i, array)) {
